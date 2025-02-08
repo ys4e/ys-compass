514dca4e566db1b3c6feae29e156a20202995532
KingRainbow44 <kobedo11@gmail.com>
2025-02-07 19:47:10 -0500

    fix(build): always target `esnext`

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(async () => ({
     envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
     build: {
         // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-        target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+        target: "esnext",
         // produce sourcemaps for debug builds
         sourcemap: !!process.env.TAURI_DEBUG,
     }
